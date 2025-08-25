1d5f148680f85ef88967f19e3671977f609cdcd9
Dhiraj2024 <dhiraj2062062004p@gmail.com>
2025-08-25 18:29:24 +0530

    Initial commit for BASE project

---
+++ beginner/b54.js
@@ -0,0 +1,41 @@
+
+
+        const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Enter a number (1–7): ", function(input) {
+  let day = parseInt(input);
+
+  switch (day) {
+    case 1:
+      console.log("Sunday");
+      break;
+    case 2:
+      console.log("Monday");
+      break;
+    case 3:
+      console.log("Tuesday");
+      break;
+    case 4:
+      console.log("Wednesday");
+      break;
+    case 5:
+      console.log("Thursday");
+      break;
+    case 6:
+      console.log("Friday");
+      break;
+    case 7:
+      console.log("Saturday");
+      break;
+    default:
+      console.log("Invalid input");
+  }
+
+  rl.close();
+});
+
+
